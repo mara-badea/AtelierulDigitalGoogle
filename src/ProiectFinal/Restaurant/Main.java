40cef4e27097422a87f2b7ccc425c103ae46cdb3
Mara Badea <badeamara2606@gmail.com>
2023-01-13 23:38:20 +0200

    pb builder

--- src/ProiectFinal/Restaurant/Main.java
+++ src/ProiectFinal/Restaurant/Main.java
@@ -1,2 +1,22 @@
-package ProiectFinal.Restaurant;public class Main {
+package ProiectFinal.Restaurant;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class Main
+{
+
+
+    public static void main(String[] args) {
+        Meal meal = new mealBuilder()
+                .buildMainDish("Hamburger")
+                .buildSideDish("Fries")
+                .buildDrink("Coke")
+                .buildHasToy(true)
+                .build();
+        System.out.println("Main Dish : " + meal.getMainDish());
+        System.out.println("Side Dish : " + meal.getSideDish());
+        System.out.println("Drink : " + meal.getDrink());
+        System.out.println("Has Toy : " + meal.isHasToy());
+    }
 }
--- src/ProiectFinal/Restaurant/Meal.java
+++ src/ProiectFinal/Restaurant/Meal.java
@@ -1,2 +1,36 @@
-package ProiectFinal.Restaurant;public class Meal {
+package ProiectFinal.Restaurant;
+
+public class Meal {
+    public String mainDish;
+    public String sideDish;
+    public String drink;
+    public boolean isHasToy;
+
+    public Meal (mealBuilder buildMeal)
+    {
+        this.mainDish = buildMeal.mainDish;
+        this.sideDish = buildMeal.sideDish;
+        this.drink = buildMeal.drink;
+        this.isHasToy = buildMeal.isHasToy;
+    }
+    public String getMainDish()
+    {
+        return this.mainDish;
+    }
+
+    public String getSideDish()
+    {
+        return this.sideDish;
+    }
+
+    public String getDrink()
+    {
+        return this.drink;
+    }
+
+    public boolean isHasToy()
+    {
+        return this.isHasToy;
+    }
+
 }
